61e35e7c5ee98ec0f47ea50017e3d8e0e36a6971
Juan Ríos <juanrios@mbp-de-juan.esrico.local>
2016-06-29 12:59:34 -0500

    Initial Commit

---
+++ CCU2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCU2016
+//
+//  Created by Juan Ríos on 29/06/16.
+//  Copyright © 2016 Esri Colombia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
